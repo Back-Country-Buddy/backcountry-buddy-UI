3fd0801e3db0b70a0f951ae61e869831aeb72833
rachelbuchta <rachelbuchta@yahoo.com>
2021-04-08 17:18:17 -0600

    Add dummy data and render it on profile with typescript

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,22 @@
 import './App.css'
 import LandingPage from '../LandingPage/LandingPage'
+import { Profile } from '../Profile/Profile'
+import { userData } from '../../UserDummyData'
+import { useState } from 'react'
 
 function App() {
+  const [user, setUser] = useState(userData)
+
   return (
     <div className="App">
       <LandingPage />
+      <Profile 
+        name={user.name} 
+        email={user.email} 
+        userName={user.userName}
+        emergencyName={user.emergencyName}
+        emergencyNumber={user.emergencyNumber}
+      />
     </div>
   );
 }
--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { FunctionComponent } from 'react'
 
-export default function Profile() {
+interface ProfileProps 
+{
+  name: string,
+  email: string,
+  userName: string,
+  emergencyName: string,
+  emergencyNumber: string 
+}
+
+export const Profile: FunctionComponent<ProfileProps> = ({name, email, userName, emergencyName, emergencyNumber}) => {
+
   return (
     <div>
-      
+      <h1>My Account</h1>
+      <img />
+      <h3 className='name'>{name}</h3>
+      <h3 className='email'>{email}</h3>
+      <h3 className='userName'>{userName}</h3>
     </div>
   )
 }
