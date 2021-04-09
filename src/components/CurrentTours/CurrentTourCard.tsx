5848a0e3752c41f305590da6f58317eda52327e9
tashiad <tashiadavis10@gmail.com>
2021-04-09 08:20:53 -0600

    Add mountain icon to cards

--- src/components/CurrentTours/CurrentTourCard.tsx
+++ src/components/CurrentTours/CurrentTourCard.tsx
@@ -8,6 +8,7 @@ interface Props {
 const CurrentTourCard: React.FC<Props> = ({ date, location }) => {
   return (
     <article>
+      <img src="https://img.icons8.com/nolan/64/mountain.png" alt="mountains icon"/>
       <h3>{location}</h3>
       <h3>{date}</h3>
     </article>
