3cbb5a2107f2111f07e4717138a246e17a09040a
Boone Epstein <booneepstein@gmail.com>
2021-04-08 21:09:38 -0500

    Add state interface

--- src/components/Form/TourForm.tsx
+++ src/components/Form/TourForm.tsx
@@ -8,12 +8,32 @@ interface TourFormProps {
 }
 
 interface TourFormState {
-
+  location: string,
+  date: number,
+  hazardWeather: string,
+  hazardAvalanche: string,
+  hazardSummary: string,
+  routePreview: string,
+  routeAlternative: string,
+  emergencyPlan: string,
+  debriefConditions: string,
+  debriefDecisions: string,
+  debriefPlan: string
 }
 
 export const TourForm: React.FC<TourFormProps> = ({ userId }) => {
   const [state, setState] = useState<TourFormState>({
-
+    location: '',
+    date: Date.now(),
+    hazardWeather: '',
+    hazardAvalanche: '',
+    hazardSummary: '',
+    routePreview: '',
+    routeAlternative: '',
+    emergencyPlan: '',
+    debriefConditions: '',
+    debriefDecisions: '',
+    debriefPlan: ''
   })
 
   return (
