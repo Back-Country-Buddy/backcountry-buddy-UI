8e2bd09b5c57f159ca039bad6cdb8819f38f32a8
tashiad <tashiadavis10@gmail.com>
2021-04-10 10:05:18 -0600

    Create landing page placeholder

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import './App.css'
-import LandingPage from '../LandingPage/LandingPage'
+import { LandingPage } from '../LandingPage/LandingPage'
 import { Profile } from '../Profile/Profile'
 import { CurrentTours } from "../CurrentTours/CurrentTours"
 import { userData } from '../../mockdata/UserDummyData'
@@ -14,7 +14,7 @@ function App() {
 
   return (
     <div className="App">
-      <LandingPage />
+      <LandingPage name={user.name} />
       <Profile
         name={user.name}
         email={user.email}
@@ -22,7 +22,8 @@ function App() {
         emergencyName={user.emergencyName}
         emergencyNumber={user.emergencyNumber}
       />
-      <CurrentTours currentTours={currentTours} /> {/* rendering this here for
+      <CurrentTours currentTours={currentTours} />{" "}
+      {/* rendering this here for
       now so I can see my changes in the browser */}
     </div>
   );
--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -1,15 +1,23 @@
-import CurrentTourCard from '../CurrentTours/CurrentTourCard'
+import { CurrentTours } from '../CurrentTours/CurrentTours'
 import TourForm from '../Form/TourForm'
 import PastTours from '../PastTours/PastTours'
 import NavBar from '../NavBar/NavBar'
 
 import React from 'react'
+import './LandingPage.css'
 
+interface Props {
+    name: string;
+}
 
-export default function LandingPage() {
+export const LandingPage: React.FC<Props> = ({ name }) => {
   return (
-    <div>
-    
-    </div>
+    <main className="landing">
+      <section className="landing-img">
+        <h1 className="logo">Backcountry Buddy</h1>
+        <h2>Welcome, {name}</h2>
+      </section>
+      <p>navbar placeholder</p>
+    </main>
   )
 }
