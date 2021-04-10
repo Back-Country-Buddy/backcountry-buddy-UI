18a264e71d29ee977b2b3cedb2b5a28ebabf7643
rachelbuchta <rachelbuchta@yahoo.com>
2021-04-09 19:41:46 -0600

    Build past tour cards and add styling

--- src/PastTourData.js
+++ src/PastTourData.js
@@ -3,21 +3,21 @@ export const tourData =
   {
     id: 4,
     date: '3/7/21',
-    location: 'Breckenridge, CO',
+    location: 'Breckenridge',
     creator_id: 1,
     complete: 'false'
   },
     {
     id: 7,
     date: '1/18/21',
-    location: 'Vail, CO',
+    location: 'Vail',
     creator_id: 1,
     complete: 'true'
   },
     {
     id: 3,
     date: '11/11/20',
-    location: 'Aspen, CO',
+    location: 'Aspen',
     creator_id: 1,
     complete: 'false'
   }
--- src/components/PastTours/PastTours.tsx
+++ src/components/PastTours/PastTours.tsx
@@ -1,41 +1,36 @@
 import React, { useState, FunctionComponent } from 'react'
-import PastTourCard from './PastTourCard'
+import { PastTourCard } from './PastTourCard'
 import SearchBar from './SearchBar'
-
+import './PastTours.css'
 
 interface pastTour {
-  id: number,
-  date: string,
-  location: string,
-  creator_id: number,
-  complete: boolean
+  id: number;
+  date: string;
+  location: string;
+  creator_id: number;
+  complete: boolean;
 }
 
 interface TourProps {
   pastTours: Array<pastTour>
 }
 
-export const PastTours: FunctionComponent<TourProps> = ({ pastTours }) => {
-
-  console.log(pastTours)
-
-  const createPastTourCards = () => {
-   return pastTours.map(tour => {
+export const PastTours: React.FC<TourProps> = ({ pastTours }) => {
+  const createPastTourCards = pastTours.map(tour => {
       return (
-      <article key={tour.id} className='tour-card'>
-        <p>{tour.date}</p>
-        <p>{tour.location}</p>
-      </article>
-        
-      )
-    })
-  }
+      <PastTourCard  
+        key={tour.id}
+        date={tour.date}
+        location={tour.location}
+      />
+    )  
+  })
 
   return (
-    <div>
+    <section className='past-tours'>
       <SearchBar />
       <h1>Past Tours</h1>
-      {createPastTourCards()}
-    </div>
+      {createPastTourCards}
+    </section>
   )
 }
