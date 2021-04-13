ba534616bc6da4ef2fef62fa8122e8e35ecb9366
tashiad <tashiadavis10@gmail.com>
2021-04-13 11:24:34 -0600

    Clean up App code

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,61 +8,48 @@ import { PastTours } from "../PastTours/PastTours";
 import { userData } from "../../mockdata/UserDummyData";
 import currentToursData from "../../mockdata/CurrentToursDummyData";
 import { tourData } from "../../mockdata/PastTourData";
-import { Route } from 'react-router-dom'
-import { PastTourDetails } from '../PastTours/PastTourDetails.tsx'
-import { NavBar } from '../NavBar/NavBar'
+import { Route } from "react-router-dom";
+import { PastTourDetails } from "../PastTours/PastTourDetails.tsx";
+import { NavBar } from "../NavBar/NavBar";
 
 function App() {
   const [user] = useState(userData);
   const [currentTours] = useState(currentToursData);
   const [pastTours] = useState(tourData);
-  // const [currentTours, setCurrentTours] = useState<object | null>(currentToursData)
-  // types currently aren't working here since this isn't a tsx file but I think we want it to be
 
   return (
     <div className="App">
+      <Route exact path="/" render={() => <LandingPage name={user.name} />} />
 
-      < Route
-        exact
-        path='/'
-        render={() =>
-          <LandingPage name={user.name} />}
-      />
-
-      < Route
-        path='/profile'
-        render={()=>
+      <Route
+        path="/profile"
+        render={() => (
           <Profile
-              name={user.name}
-              email={user.email}
-              userName={user.userName}
-              emergencyName={user.emergencyName}
-              emergencyNumber={user.emergencyNumber}
-          />}
+            name={user.name}
+            email={user.email}
+            userName={user.userName}
+            emergencyName={user.emergencyName}
+            emergencyNumber={user.emergencyNumber}
+          />
+        )}
       />
 
-      < Route
-        path='/add-tour'
-        render={()=>
-         <TourForm userId={userData.id} />}
+      <Route
+        path="/add-tour"
+        render={() => <TourForm userId={userData.id} />}
       />
 
-      < Route
-        path='/current-tours'
-        render={()=>
-         <CurrentTours currentTours={currentTours} />}
+      <Route
+        path="/current-tours"
+        render={() => <CurrentTours currentTours={currentTours} />}
       />
 
-      < Route
-        path='/past-tours'
-        render={()=>
-          <PastTours pastTours={pastTours} />}
+      <Route
+        path="/past-tours"
+        render={() => <PastTours pastTours={pastTours} />}
       />
 
-      < Route
-        path='/tour-details/:id'
-        component={ PastTourDetails }
-      />
+      <Route path="/tour-details/:id" component={PastTourDetails} />
       <NavBar />
     </div>
   );
