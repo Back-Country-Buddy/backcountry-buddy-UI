5338807571e5142ef119048ab8ac63b3381c8bb0
Boone Epstein <booneepstein@gmail.com>
2021-04-13 14:49:25 -0500

    Add rendering for new components and clean up naming

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { LoginButton } from '../Login/LoginButton'
+import { LogoutButton } from '../Login/LogoutButton'
+import { useAuth0 } from "@auth0/auth0-react"
 import './LandingPage.css'
 
 interface Props {
@@ -6,12 +9,22 @@ interface Props {
 }
 
 export const LandingPage: React.FC<Props> = ({ name }) => {
+  const { user, isAuthenticated, isLoading } = useAuth0()
+
   return (
     <main className="landing">
       <header className="landing-img">
         <h1 className="logo">Backcountry Buddy</h1>
         <h2>Welcome, {name}</h2>
       </header>
+      {
+        isAuthenticated ?
+        <>
+          <LogoutButton />
+          {user.name}
+        </> :
+        <LoginButton />
+      }
     </main>
   )
 }
