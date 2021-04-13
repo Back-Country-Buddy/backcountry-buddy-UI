f893bdc4370cc0a16aa1a809faf5a8dfe2167cba
Boone Epstein <booneepstein@gmail.com>
2021-04-13 14:39:16 -0500

    Wrap app call in auth0

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
 import App from './components/App/App'
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals'
 import { BrowserRouter } from 'react-router-dom'
+import { Auth0Provider } from "@auth0/auth0-react"
 
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Auth0Provider
+      domain="dev-bckux7co.us.auth0.com"
+      clientId="xq75vrn6mmA5fnAI7hk1Hf39lVf6RNLG"
+      redirectUri={window.location.origin}
+    >
+      <App />
+    </Auth0Provider>
   </BrowserRouter>,
   document.getElementById('root')
 );
