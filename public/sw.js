13dc119b03fc780d16ca5db924ca3d1f1eed1749
rachelbuchta <rachelbuchta@yahoo.com>
2021-04-13 15:06:57 -0600

    Add service worker files and test it with lighthouse

---
+++ public/sw.js
@@ -0,0 +1,39 @@
+const pwaCache = 'pwa-cache-1'
+const staticCache = [
+      '/index.html',
+      '/index.css',
+      '/index.js'
+]
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(pwaCache)
+      .then(cache => cache.addAll(staticCache))
+  )
+})
+
+self.addEventListener('activate', (e) => {
+  let cacheCleaned = caches.keys()
+    .then(keys => {
+      keys.forEach(key => {
+        if (key !== pwaCache) {
+          return caches.delete(key)
+        }
+      })
+    })
+  e.waitUntil(cacheCleaned)
+})
+
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      if (res) return res
+
+      return fetch(e.request).then((newRes) => {
+        caches.open(pwaCache).then((cache) => cache.put(e.request, newRes))
+        return newRes.clone()
+      })
+    })
+  )
+  })
