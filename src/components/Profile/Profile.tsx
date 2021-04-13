235dc086a7bde6280c7760dec0d5eb52aeec41a7
Boone Epstein <booneepstein@gmail.com>
2021-04-13 14:49:51 -0500

    Call auth0 objects in profile

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,23 +1,25 @@
 import React, { FunctionComponent } from 'react'
+import { useAuth0 } from "@auth0/auth0-react"
 
-interface ProfileProps 
+interface ProfileProps
 {
   name: string,
   email: string,
   userName: string,
   emergencyName: string,
-  emergencyNumber: string 
+  emergencyNumber: string
 }
 
 
 export const Profile: FunctionComponent<ProfileProps> = ({name, email, userName, emergencyName, emergencyNumber}) => {
+  const { user, isAuthenticated, isLoading } = useAuth0()
 
   return (
     <div>
       <h1>My Account</h1>
-      <img src="https://img.icons8.com/fluent/50/000000/user-male-circle.png" alt="profile placeholder" />
-      <h3 className='name'>{name}</h3>
-      <h3 className='email'>{email}</h3>
+      <img src={user.picture} alt={user.name} />
+      <h3 className='name'>{user.name}</h3>
+      <h3 className='email'>{user.email}</h3>
       <h3 className='userName'>{userName}</h3>
     </div>
   )
