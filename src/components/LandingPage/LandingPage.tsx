427ba73ab84a9626086c9ad4779b0ba8fabbed3f
Boone Epstein <booneepstein@gmail.com>
2021-04-13 15:11:21 -0500

    Clean up relevant components

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -3,21 +3,17 @@ import { LoginButton } from '../Login/LoginButton'
 import { useAuth0 } from "@auth0/auth0-react"
 import './LandingPage.css'
 
-interface Props {
-    name: string;
-}
 
-export const LandingPage: React.FC<Props> = ({ name }) => {
-  const { user, isAuthenticated, isLoading } = useAuth0()
+export const LandingPage: React.FC = () => {
+  const { user, isAuthenticated } = useAuth0()
 
   return (
     <main className="landing">
       <header className="landing-img">
         <h1 className="logo">Backcountry Buddy</h1>
       {isAuthenticated ?
-        <h2>Welcome, {user.name}</h2> :
-        <LoginButton />
-      }
+        <h2>Welcome, {user.given_name}</h2> :
+        <LoginButton />}
       </header>
     </main>
   )
--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -2,18 +2,10 @@ import React, { FunctionComponent } from 'react'
 import { LogoutButton } from '../Login/LogoutButton'
 import { useAuth0 } from "@auth0/auth0-react"
 
-interface ProfileProps
-{
-  name: string,
-  email: string,
-  userName: string,
-  emergencyName: string,
-  emergencyNumber: string
-}
 
 
-export const Profile: FunctionComponent<ProfileProps> = ({name, email, userName, emergencyName, emergencyNumber}) => {
-  const { user, isAuthenticated, isLoading } = useAuth0()
+export const Profile: FunctionComponent = () => {
+  const { user } = useAuth0()
 
   return (
     <div>
@@ -21,7 +13,7 @@ export const Profile: FunctionComponent<ProfileProps> = ({name, email, userName,
       <img src={user.picture} alt={user.name} />
       <h3 className='name'>{user.name}</h3>
       <h3 className='email'>{user.email}</h3>
-      <h3 className='userName'>{userName}</h3>
+      <h3 className='userName'>{user.nickname}</h3>
       <LogoutButton />
     </div>
   )
