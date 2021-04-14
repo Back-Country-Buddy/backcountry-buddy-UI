a016e96655d79692bf01997a69cd566605fff25d
rachelbuchta <rachelbuchta@yahoo.com>
2021-04-14 08:38:37 -0600

    Change starturl in manifest to index.html to fix pwa error

--- src/index.js
+++ src/index.js
@@ -20,12 +20,12 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// if (navigator.serviceWorker) {
-//     navigator.serviceWorker.register('../../sw.js')
-//         .then((registration) => {
-//             console.log('SW Registered', registration);
-//         });
-// }
+if (navigator.serviceWorker) {
+    navigator.serviceWorker.register('../../sw.js')
+        .then((registration) => {
+            console.log('SW Registered', registration);
+        });
+}
 
 // if (window.Cypress) {
 //     serviceWorkerRegistration.unregister();
