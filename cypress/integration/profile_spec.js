651e9f8f8bfb6e7cc9b4c439e12fd6274b77d8a7
tashiad <tashiadavis10@gmail.com>
2021-04-13 19:26:39 -0600

    Add login functionality to Profile test file

--- cypress/integration/profile_spec.js
+++ cypress/integration/profile_spec.js
@@ -1,21 +1,57 @@
-const baseUrl = "http://localhost:3000/profile";
+describe("Profile Page", () => {
+  const baseUrl = "http://localhost:3000"
+  const profileUrl = "http://localhost:3000/profile"
 
-describe.skip("Profile Page", () => {
-  it("Should have correct header", () => {
-    cy.visit(baseUrl).get("h1").should("have.text", "My Account");
-  });
+  const logOut = () => {
+    cy.get(".main-menu")
+      .contains("Profile")
+      .click()
+      .get("button")
+      .contains("Log Out")
+      .click()
+  }
 
-  it("Should render a placeholder profile icon", () => {
-    cy.visit(baseUrl).get('[alt="profile placeholder"]').should("be.visible");
-  });
-
-  it("Should display user name, email, and username", () => {
+  beforeEach(() => {
     cy.visit(baseUrl)
-      .get(".name")
-      .should("have.text", "Rachel")
+      .get("header button")
+      .click()
+      .get("form input[name=username]")
+      .type("BCBTestUser@gmail.com")
+      .get("form input[name=password]")
+      .type("BCBtest123!")
+      .get("button[name=action]")
+      .click()
+      .get(".main-menu")
+      .contains("Profile")
+      .click()
+  })
+
+  it("Should visit the correct URL path on click", () => {
+    cy.url().should("include", profileUrl)
+
+    logOut()
+  })
+
+  it("Should have correct header", () => {
+    cy.get("h1").should("have.text", "My Account")
+
+    logOut()
+  })
+
+  it("Should render a profile icon", () => {
+    cy.get('[alt="Test User"]').should("be.visible")
+
+    logOut()
+  })
+
+  it("Should display name, email, and username", () => {
+    cy.get(".name")
+      .should("have.text", "Test User")
       .get(".email")
-      .should("have.text", "user@user.com")
+      .should("have.text", "bcbtestuser@gmail.com")
       .get(".userName")
-      .should("have.text", "userName");
-  });
-});
+      .should("have.text", "bcbtestuser")
+
+    logOut()
+  })
+})
