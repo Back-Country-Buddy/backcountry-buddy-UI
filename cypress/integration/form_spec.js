8fb9239f3816713d64f2716d4c50de6fe3e3a96b
tashiad <tashiadavis10@gmail.com>
2021-04-13 19:13:12 -0600

    Add login functionality to Form tests

--- cypress/integration/form_spec.js
+++ cypress/integration/form_spec.js
@@ -1,9 +1,39 @@
-const baseUrl = "http://localhost:3000/add-tour";
+describe("Add Tour Form Page", () => {
+  const baseUrl = "http://localhost:3000"
+  const tourUrl = "http://localhost:3000/add-tour"
 
-describe.skip("Add Tour Form Page", () => {
-  it("Should have correct headers", () => {
+  const logOut = () => {
+    cy.get(".main-menu")
+      .contains("Profile")
+      .click()
+      .get("button")
+      .contains("Log Out")
+      .click()
+  }
+
+  beforeEach(() => {
     cy.visit(baseUrl)
-      .get(".section-title")
+      .get("header button")
+      .click()
+      .get("form input[name=username]")
+      .type("BCBTestUser@gmail.com")
+      .get("form input[name=password]")
+      .type("BCBtest123!")
+      .get("button[name=action]")
+      .click()
+      .get(".main-menu")
+      .contains("Add Tour")
+      .click()
+  })
+
+  it("Should take you to the correct URL path", () => {
+    cy.url().should("include", tourUrl)
+
+    logOut()
+  })
+
+  it("Should have correct headers", () => {
+    cy.get(".section-title")
       .eq(0)
       .find("h3")
       .should("have.text", "Assemble Your Group")
@@ -18,24 +48,30 @@ describe.skip("Add Tour Form Page", () => {
       .get(".section-title")
       .eq(3)
       .find("h3")
-      .should("have.text", "Discuss Your Emergency Plan");
-  });
+      .should("have.text", "Discuss Your Emergency Plan")
+
+    logOut()
+  })
+
   it("Should have a date and location field", () => {
-    cy.visit(baseUrl)
-      .get('input[type="date"]')
+    cy.get('input[type="date"]')
       .get('input[type="text"]')
       .eq(0)
-      .should("have.text", "");
-  });
+      .should("have.text", "")
+
+    logOut()
+  })
+
   it("Should start out with 3 unchecked check boxes", () => {
-    cy.visit(baseUrl)
-      .get('input[type="checkbox"]')
+    cy.get('input[type="checkbox"]')
       .should("have.length", 8)
-      .should("not.be.checked");
-  });
+      .should("not.be.checked")
+
+    logOut()
+  })
+
   it("Should check the Anticipate the Hazard checkbox if all section inputs have text", () => {
-    cy.visit(baseUrl)
-      .get('input[type="text"]')
+    cy.get('input[type="text"]')
       .eq(1)
       .type("testing 1")
       .get('input[type="text"]')
@@ -46,11 +82,13 @@ describe.skip("Add Tour Form Page", () => {
       .type("testing 3")
       .get('input[type="checkbox"]')
       .eq(0)
-      .should("be.checked");
-  });
+      .should("be.checked")
+
+    logOut()
+  })
+
   it("Should check the Plan Your Route checkbox if all section inputs have text", () => {
-    cy.visit(baseUrl)
-      .get('input[type="text"]')
+    cy.get('input[type="text"]')
       .eq(5)
       .type("testing 4")
       .get('input[type="text"]')
@@ -58,22 +96,19 @@ describe.skip("Add Tour Form Page", () => {
       .type("testing 5")
       .get('input[type="checkbox"]')
       .eq(2)
-      .should("be.checked");
-  });
+      .should("be.checked")
+
+    logOut()
+  })
+
   it("Should check the Discuss your emergency plan checkbox if all section inputs have text", () => {
-    cy.visit(baseUrl)
-      .get('input[type="text"]')
+    cy.get('input[type="text"]')
       .eq(7)
       .type("testing 6")
       .get('input[type="checkbox"]')
       .eq(3)
-      .should("be.checked");
-  });
-});
+      .should("be.checked")
 
-
-
-
-
-
-
+    logOut()
+  })
+})
