2c36a74bc0e64a1aa12bdc3ce2929d2e0f78cebd
tashiad <tashiadavis10@gmail.com>
2021-04-20 10:03:28 -0600

    Style delete button

--- src/components/PastTours/PastTourCard.tsx
+++ src/components/PastTours/PastTourCard.tsx
@@ -7,30 +7,34 @@ interface TourProps {
   tourId: number
   date: string
   location: string
-  removeTour: (tourId:number) => any
+  removeTour: (tourId: number) => any
 }
 
-export const PastTourCard: React.FC<TourProps> = ({ userId, date, location, tourId, removeTour }) => {
-
+export const PastTourCard: React.FC<TourProps> = ({
+  userId,
+  date,
+  location,
+  tourId,
+  removeTour,
+}) => {
   return (
-    <article className='card-wrapper'>
-      <div
-      className='delete-wrapper'
-      onClick={()=>removeTour(tourId)}
-      >X
-      </div>
-      <Link style={{textDecoration: 'none'}} to={`/past-tours/${userId}/${tourId}/${location}/${date}`}>
-        <article className="tour-card">
-          <img
-            src="https://img.icons8.com/nolan/64/mountain.png"
-            alt="mountains icon"
-          />
-          <div className="card-info">
-            <h3>{location}</h3>
-            <p>{date}</p>
-          </div>
-        </article>
-      </Link>
-    </article>
+    <Link
+      style={{ textDecoration: "none" }}
+      to={`/past-tours/${userId}/${tourId}/${location}/${date}`}
+    >
+      <article className="tour-card">
+        <img
+          src="https://img.icons8.com/nolan/64/mountain.png"
+          alt="mountains icon"
+        />
+        <div className="card-info">
+          <h3>{location}</h3>
+          <p>{date}</p>
+        </div>
+        <button className="delete" onClick={() => removeTour(tourId)}>
+          X
+        </button>
+      </article>
+    </Link>
   )
 }
