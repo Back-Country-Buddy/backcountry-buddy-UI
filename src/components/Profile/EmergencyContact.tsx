cc8b715212fa9edbc2895f45a38d97072f0eb168
tashiad <tashiadavis10@gmail.com>
2021-05-03 13:33:58 -0600

    Make emergency contact form its own component

---
+++ src/components/Profile/EmergencyContact.tsx
@@ -0,0 +1,78 @@
+import React from "react"
+import { useAuth0 } from "@auth0/auth0-react"
+import "./Profile.css"
+import { updateUser } from "../../apiRequests/userRequests.js"
+
+interface EmergencyProps {
+  user: {
+    id: string
+    user_name: string
+    email_address: string
+    emergency_contact_name: string
+    emergency_number: string
+    last_name: string
+    first_name: string
+    full_name: string
+    picture: string
+  }
+  setUser: React.Dispatch<React.SetStateAction<any>>
+}
+
+export const EmergencyContact: React.FC<EmergencyProps> = ({
+  user,
+  setUser,
+}) => {
+  const { getAccessTokenSilently } = useAuth0()
+
+  const submitInfo = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault()
+    getAccessTokenSilently().then((token) => {
+      updateUser(token, user.id, {
+        emergency_contact_name: user.emergency_contact_name,
+        emergency_number: user.emergency_number,
+      })
+    })
+  }
+
+  return (
+    <form className="emergency-form">
+      <h3>Emergency Contact</h3>
+      <div className="emergency-form-section">
+        <label htmlFor="emergencyName" className="emergency-label">
+          Name:
+        </label>
+        <input
+          type="text"
+          name="emergencyName"
+          value={user.emergency_contact_name}
+          onChange={(e) =>
+            setUser({
+              ...user,
+              emergency_contact_name: e.target.value,
+            })
+          }
+        />
+      </div>
+      <div className="emergency-form-section">
+        <label htmlFor="phone" className="emergency-label">
+          Phone:
+        </label>
+        <input
+          type="text"
+          name="phone"
+          placeholder="123-456-7890"
+          value={user.emergency_number}
+          onChange={(e) =>
+            setUser({
+              ...user,
+              emergency_number: e.target.value,
+            })
+          }
+        />
+      </div>
+      <button className="button-save" onClick={(e) => submitInfo(e)}>
+        <strong>Save</strong>
+      </button>
+    </form>
+  )
+}
--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,9 +1,8 @@
 import React from "react"
-import { useAuth0 } from "@auth0/auth0-react"
 import "./Profile.css"
-import { updateUser } from "../../apiRequests/userRequests.js"
 import { LogoutButton } from "../Login/LogoutButton"
 import { NavBar } from "../NavBar/NavBar"
+import { EmergencyContact } from "./EmergencyContact"
 
 interface ProfileProps {
   user: {
@@ -21,18 +20,6 @@ interface ProfileProps {
 }
 
 export const Profile: React.FC<ProfileProps> = ({ user, setUser }) => {
-  const { getAccessTokenSilently } = useAuth0()
-
-  const submitInfo = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    e.preventDefault()
-    getAccessTokenSilently().then((token) => {
-      updateUser(token, user.id, {
-        emergency_contact_name: user.emergency_contact_name,
-        emergency_number: user.emergency_number,
-      })
-    })
-  }
-
   return (
     <main className="background profile-background-img">
       <div className="sub-container">
@@ -44,56 +31,18 @@ export const Profile: React.FC<ProfileProps> = ({ user, setUser }) => {
         />
 
         <div>
-          <p className='name'>
+          <p className="name">
             <strong>Name:</strong> {user.full_name}
           </p>
-          <p className='userName'>
+          <p className="userName">
             <strong>Username:</strong> {user.user_name}
           </p>
-          <p className='email'>
+          <p className="email">
             <strong>Email:</strong> {user.email_address}
           </p>
         </div>
 
-        <form className="emergency-form">
-          <h3>Emergency Contact</h3>
-          <div className="emergency-form-section">
-            <label htmlFor="emergencyName" className="emergency-label">
-              Name:
-            </label>
-            <input
-              type="text"
-              name="emergencyName"
-              value={user.emergency_contact_name}
-              onChange={(e) =>
-                setUser({
-                  ...user,
-                  emergency_contact_name: e.target.value,
-                })
-              }
-            />
-          </div>
-          <div className="emergency-form-section">
-            <label htmlFor="phone" className="emergency-label">
-              Phone:
-            </label>
-            <input
-              type="text"
-              name="phone"
-              placeholder="123-456-7890"
-              value={user.emergency_number}
-              onChange={(e) =>
-                setUser({
-                  ...user,
-                  emergency_number: e.target.value,
-                })
-              }
-            />
-          </div>
-          <button className="button-save" onClick={(e) => submitInfo(e)}>
-            <strong>Save</strong>
-          </button>
-        </form>
+        <EmergencyContact user={user} setUser={setUser} />
 
         <LogoutButton />
       </div>
