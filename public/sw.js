d46368c0d3ff70644d92e3183281da7449754a3b
Boone Epstein <booneepstein@gmail.com>
2021-05-06 10:31:22 -0500

    Change sw file

--- public/sw.js
+++ public/sw.js
@@ -1,42 +1,22 @@
-const CACHE_NAME = 'backcountry-buddy-task-manager';
-const appShellFiles = [
-  '/',
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Opened cache');
-        return cache.addAll(appShellFiles);
-      })
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
-  );
-});
-
-self.addEventListener('activate', event => {
-  const cacheWhitelist = ['backcountry-buddy-task-manager'];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.open(event.request.url).then(function(cache) {
+        return cache.match(event.request).then(function (response) {
+          if(navigator.onLine){
+            return fetch(event.request).then(function(response) {
+              if(event.request.method == 'GET'){
+                cache.put(event.request, response.clone());
+              }
+              return response;
+            });
+          }else{
+            if(response){
+              return response
+            }else{
+              return null
+            }
           }
-        })
-      );
-    })
-  );
+        });
+      })
+    );
 });
