d8358ace6d2a193f55e75fb965c7f220b8d43bf0
tashiad <tashiadavis10@gmail.com>
2021-05-06 15:47:12 -0600

    Add placeholder elements and pseudocode for next steps

--- src/components/Form/Ride.tsx
+++ src/components/Form/Ride.tsx
@@ -14,7 +14,15 @@ export const Ride: React.FC<RideProps> = ({ setChecked, isChecked }) => {
         <img src={route} alt="route" className="form-icon" />
         <h2 className="title">RIDE safely</h2>
       </div>
-      <button className="button-secondary">Get Directions to the Trailhead</button>
+
+      {/* Need to get coordinates from tour in order for this to work
+      Will require a conditional in case coordinates haven't been added yet */}
+      {/* <a href={`https://www.google.com/maps/search/?api=1&query=${lat},${lon}`}>
+        <button className="button-secondary">
+          Get Directions to the Trailhead
+        </button>
+      </a> */}
+
       <div className="section-title section-title-ride">
         <label htmlFor="ride-checkbox" className="hidden-label">
           Completed
